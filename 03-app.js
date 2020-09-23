65e33aad41f8bb68e1e33c4539598e69d334a11f
George Pontier <gpontierh@gmail.com>
2020-09-22 23:02:13 -0400

    modificacion

--- 03-app.js
+++ 03-app.js
@@ -1,17 +1,17 @@
-// algoritmo bebe 1
-    // var array = [5,4,1,2,3];
-    // if (array[0]>array[1]){
-    //     array[0]=1;
-    //     array[1]=2;
-    //     array[2]=3;
-    //     array[3]=4;
-    //     array[4]=5;
-    //     console.log(array)
-    // } else {
-    //     console.log('todo bien');
-    // };
+algoritmo bebe 1
+    var array = [5,4,1,2,3];
+    if (array[0]>array[1]){
+        array[0]=1;
+        array[1]=2;
+        array[2]=3;
+        array[3]=4;
+        array[4]=5;
+        console.log(array)
+    } else {
+        console.log('todo bien');
+    };
 
-//algoritmo bebe 2
+algoritmo bebe 2
 
 for (i=1; i<=6; i++){
     for(j=1; j<=6; j++){ }
