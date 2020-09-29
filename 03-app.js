ba8dc3a2a25b0766e4891735831f6a2ca97d2e05
George Pontier <gpontierh@gmail.com>
2020-09-29 15:41:29 -0400

    archivo html para practica

--- 03-app.js
+++ 03-app.js
@@ -1,21 +1,42 @@
-yo lo cambie primero
+// algoritmo bebe 1
+    // var array = [5,4,1,2,3];
+    // if (array[0]>array[1]){
+    //     array[0]=1;
+    //     array[1]=2;
+    //     array[2]=3;
+    //     array[3]=4;
+    //     array[4]=5;
+    //     console.log(array)
+    // } else {
+    //     console.log('todo bien');
+    // };
 
-algoritmo bebe 1
-    var array = [5,4,1,2,3];
-    if (array[0]>array[1]){
-        array[0]=1;
-        array[1]=2;
-        array[2]=3;
-        array[3]=4;
-        array[4]=5;
-        console.log(array)
-    } else {
-        console.log('todo bien');
+//algoritmo bebe 2
+// var numeros = new Array (1,2,3,4,5,6)
+// for (i=0; i<numeros.length; i++){
+//         console.log(numeros[i]);
+//         for (j=0; j<numeros.length; j++)
+//         console.log(numeros[j])
+//     };
+
+    //algoritmo bebe 2
+    alert('hola')
+let numeros2= "";
+let numeros = [1,2,3,4,5,6]
+for (i=0; i<numeros.length; i++){
+        for (j=0; j<numeros.length; j++){
+            numeros2 = numeros[j];
+        };
+       console.log(numeros[i] + " " + numeros2);
     };
+    
+// let array = [1,2,3,4,5,6];
+// let string = '';
 
-algoritmo bebe 2
+// for(i=0; i<array.length; i++){
+//     console.log(array[i]);
+//     array.forEach(element => string += element);
+    
+//     console.log(string);
+// }
 
-for (i=1; i<=6; i++){
-    for(j=1; j<=6; j++){ }
-console.log(i,j)
-}
